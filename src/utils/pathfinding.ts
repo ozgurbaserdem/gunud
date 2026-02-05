bdf87c202bfd500bebb695f0bbb27cd0de0c7cbc
Özgür Baserdem <gurr__@hotmail.com>
2026-02-06 00:17:40 +0100

    refactor: deduplicate BFS by delegating to shared calculateDistances

--- src/utils/pathfinding.ts
+++ src/utils/pathfinding.ts
@@ -1,28 +1,12 @@
 import type { Room } from '../types';
+import { calculateDistances } from './dungeonGenerator';
 
 // BFS to calculate shortest path distances from treasure room to all rooms
 export function calculateDistancesToTreasure(
   rooms: Room[],
   treasureId: number
 ): Map<number, number> {
-  const distances = new Map<number, number>();
-  const queue: number[] = [treasureId];
-  distances.set(treasureId, 0);
-
-  while (queue.length > 0) {
-    const currentId = queue.shift()!;
-    const currentDist = distances.get(currentId)!;
-    const currentRoom = rooms.find((r) => r.id === currentId)!;
-
-    for (const neighborId of currentRoom.connections) {
-      if (!distances.has(neighborId)) {
-        distances.set(neighborId, currentDist + 1);
-        queue.push(neighborId);
-      }
-    }
-  }
-
-  return distances;
+  return calculateDistances(rooms, treasureId);
 }
 
 // Get adjacent rooms (connected rooms) for a given room
